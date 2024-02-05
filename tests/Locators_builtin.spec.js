e55bdbcd32bfdd2b012435090cbac0333f364683
victor.popa <popa.georgiani93@gmail.com>
2024-02-05 21:18:46 +0200

    get

--- tests/Locators_builtin.spec.js
+++ tests/Locators_builtin.spec.js
@@ -1,4 +1,3 @@
-import {test, expect} from '@playwright/test'
 
 /*
 page.getByRole() to locate by explicit and implicit accessibility attributes.
@@ -8,15 +7,29 @@ page.getByPlaceholder() to locate an input by placeholder.
 page.getByAltText() to locate an element, usually image, by its text alternative.
 page.getByTitle() to locate an element by its title attribute.
 page.getByTestId() to locate an element based on its data-testid attribute
-
 */
 
+import {test, expect} from '@playwright/test'
 
 test('Built-inLocators' , async ({page}) => {
     
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-
+   
+   //page.getByAltText() to locate an element, usually image, by its text alternative.
    const logo =  await page.getByAltText('company-branding')
    await expect(logo).toBeVisible()
-    
+
+
+   //page.getByPlaceholder() to locate an input by placeholder
+   await page.getByPlaceholder('Username').fill('Admin')
+   await page.getByPlaceholder('Password').fill('admin123')
+   
+   //page.getByRole() to locate by explicit and implicit accessibility attributes
+   await page.getByRole('button', {type : 'submit'}).click()
+   
+   //page.getByText() to locate by text content
+   await expect(await page.getByText('Sajag Karki')).toBeVisible()
+  
+
 })
+
