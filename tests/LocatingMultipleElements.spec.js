9ccd2621b1a220746bc928600e0db51f2d8078ea
victor.popa <popa.georgiani93@gmail.com>
2024-02-05 20:54:55 +0200

    locators

--- tests/LocatingMultipleElements.spec.js
+++ tests/LocatingMultipleElements.spec.js
@@ -1,11 +1,11 @@
-// import {test, expect} from '@playwright/test'
-const {test, expect} = require('@playwright/test');
+import {test, expect} from '@playwright/test'
+
 
 test('LocatingMultipleElements' , async ({page}) => {
     
     await page.goto('https://www.demoblaze.com/index.html')
     
-    // return all links
+    //return all links
     const links = await page.$$('a') 
    
     for(const link of links)
@@ -13,7 +13,19 @@ test('LocatingMultipleElements' , async ({page}) => {
         const linktext = await link.textContent()
         console.log(linktext)
     }
+    
+    // Locate all the products displayed on home page
 
+    // wait all the products are displayed 
+    await page.waitForSelector("//div[@id='tbodyid']//h4/a") 
+
+    const products =  await page.$$("//div[@id='tbodyid']//h4/a")
+
+    for(const product of products)
+    {
+       const productName = await product.textContent()
+       console.log(productName)
+    }
 
 
 
