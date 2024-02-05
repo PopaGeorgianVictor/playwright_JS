57d5971554156a8c99c7dd57bcf5bd4943e9d58f
victor.popa <popa.georgiani93@gmail.com>
2024-02-05 19:48:14 +0200

    add await

--- tests/LocatingMultipleElements.spec.js
+++ tests/LocatingMultipleElements.spec.js
@@ -1,17 +1,20 @@
-import {test, expect} from '@playwright/test'
+// import {test, expect} from '@playwright/test'
+const {test, expect} = require('@playwright/test');
 
 test('LocatingMultipleElements' , async ({page}) => {
     
     await page.goto('https://www.demoblaze.com/index.html')
     
     // return all links
-    const links = page.$$('a') 
-
+    const links = await page.$$('a') 
+   
     for(const link of links)
     {
         const linktext = await link.textContent()
         console.log(linktext)
     }
+
+
 
 
 })
