1f9b1f00c5284a12655f2de5c85d9cfc147db924
victor.popa <popa.georgiani93@gmail.com>
2024-02-12 22:08:35 +0200

    hid

--- tests/HiddenDropDown.spec.js
+++ tests/HiddenDropDown.spec.js
@@ -3,10 +3,28 @@ import {test, expect} from '@playwright/test'
 
 test('hidden option dropdowns' , async ({page}) => {
 
-    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList')
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
+    await page.getByPlaceholder('Username').fill('Admin')
+    await page.getByPlaceholder('Password').fill('admin123')
+    await page.locator("[type='submit']").click()
 
+    await page.locator("//span[normalize-space()='PIM']").click()
 
-    await page.waitForTimeout(5000)
+    // click on dropdown
+    await page.locator('form.i').nth(2).click()
 
+//     //waiting for option
+//     await page.waitForTimeout(3000)
+
+//     const options = await page.$$("//div[@role='listbox']//span")
+
+//     for(let option of options)
+//     {
+//       const jobTitle = await option.textContent()
+//       console.log(jobTitle)
+
+    
+//     }
+await page.waitForTimeout(3000)
 })
