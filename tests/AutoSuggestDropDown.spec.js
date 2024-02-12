3e71e912cf20a997033375a158cfc1ca88274421
victor.popa <popa.georgiani93@gmail.com>
2024-02-12 21:15:08 +0200

    autosuggest

---
+++ tests/AutoSuggestDropDown.spec.js
@@ -0,0 +1,23 @@
+import {test, expect} from '@playwright/test'
+
+
+test('bootstrap dropdowns' , async ({page}) => {
+
+    await page.goto('https://www.cfrcalatori.ro/')
+    await page.locator('#input-station-departure-name').fill('bucu')
+    await page.waitForSelector("//li[contains(@class,'ui-menu-item')]/div")
+
+    const cityOption = await page.$$("//li[contains(@class,'ui-menu-item')]/div")
+    for(let option of cityOption)
+    {
+        const value = await option.textContent()
+        // console.log(value)
+        if(value.includes('București Nord'))
+        {
+           await option.click()
+           break
+        }
+    }
+
+    await page.waitForTimeout(5000)
+})
