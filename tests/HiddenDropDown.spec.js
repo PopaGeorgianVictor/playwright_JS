51e50ad8b3456f2238a400ad48befbfcf94cf6cf
victor.popa <popa.georgiani93@gmail.com>
2024-02-13 19:45:15 +0200

    hidden

--- tests/HiddenDropDown.spec.js
+++ tests/HiddenDropDown.spec.js
@@ -9,22 +9,27 @@ test('hidden option dropdowns' , async ({page}) => {
     await page.getByPlaceholder('Password').fill('admin123')
     await page.locator("[type='submit']").click()
 
-    await page.locator("//span[normalize-space()='PIM']").click()
+    // await page.locator("//span[normalize-space()='PIM']").click()
+    await page.getByRole('link', { name: 'PIM' }).click()
+    await page.getByText('-- Select --').nth(1).click();
 
     // click on dropdown
     await page.locator('form.i').nth(2).click()
 
-//     //waiting for option
-//     await page.waitForTimeout(3000)
+    //waiting for option
+    await page.waitForTimeout(3000)
 
-//     const options = await page.$$("//div[@role='listbox']//span")
+    const options = await page.$$("//div[@role='listbox']//span")
 
-//     for(let option of options)
-//     {
-//       const jobTitle = await option.textContent()
-//       console.log(jobTitle)
+    for(let option of options)
+    {
+      const jobTitle = await option.textContent()
+      console.log(jobTitle)
+      if(jobTitle.includes('QA Enginner'))
+      await option.click()
+      break
 
     
-//     }
+    }
 await page.waitForTimeout(3000)
 })
