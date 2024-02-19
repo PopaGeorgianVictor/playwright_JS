546751d94cf5ec8ac3fa1b8c4f509e85bd9d99e0
victor.popa <popa.georgiani93@gmail.com>
2024-02-19 20:23:00 +0200

    table

--- tests/Table.spec.js
+++ tests/Table.spec.js
@@ -18,33 +18,72 @@ test('handling table' , async ({page}) => {
 
     //select checkbox for one product
 
-//     const machedRow= rows.filter({
-//         has: page.locator('td'),
-//         hasText: 'Product 4' 
-//     })
+    // const machedRow= rows.filter({
+    //     has: page.locator('td'),
+    //     hasText: 'Product 4' 
+    // })
 
 
-//     await machedRow.locator('input').check()
+    // await machedRow.locator('input').check()
 
-    //3) select multiple products by re-usable function
-    await selectProduct(rows, page, 'Product 1')
-    await selectProduct(rows, page, 'Product 3')
-    await selectProduct(rows, page, 'Product 5')
+    //select multiple products by re-usable function
+    // await selectProduct(rows, page, 'Product 1')
+    // await selectProduct(rows, page, 'Product 3')
+    // await selectProduct(rows, page, 'Product 5')
 
-    await page.waitForTimeout(5000)
+    //print all product detalis using loop
 
-})
+    // for(let i=0;i<await rows.count(); i++)
+    // {
+    //     const row = rows.nth(i)
+    //     const tds = row.locator('td')
 
-    async function selectProduct(rows, page, name)
+    //     for(let j=0;j< await tds.count()-1;j++)
+    //     {
+    //         console.log(await tds.nth(j).textContent())
+    //     }
+    // }
+    
+
+    //read data from all the pages in the pages
+    const pages = await page.locator('.pagination li a')
+    console.log('Number of pages in the table:', await pages.count())
+
+    for(let p=0 ; p<await pages.count(); p++)
     {
+       if(p>0)
+        {
+        await pages.nth(p).click()
+        }
+        for(let i=0;i<await rows.count(); i++)
+        {
+            const row = rows.nth(i)
+            const tds = row.locator('td')
 
-        const machedRow= rows.filter({
-        has: page.locator('td'),
-        hasText: name
-        })
-        await machedRow.locator('input').check()
+            for(let j=0;j<await tds.count()-1;j++)
+            {
+                console.log(await tds.nth(j).textContent())
 
+            }
+            
+       }
+        
     }
+        
+})
+    
+
+
+//     async function selectProduct(rows, page, name)
+//     {
+
+//         const machedRow= rows.filter({
+//         has: page.locator('td'),
+//         hasText: name
+//         })
+//         await machedRow.locator('input').check()
+
+//     }
 
 
 
