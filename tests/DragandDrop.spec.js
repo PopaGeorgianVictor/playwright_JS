495461983bdc08e44977ef9fd2631a16b4088d83
victor.popa <popa.georgiani93@gmail.com>
2024-02-20 21:27:58 +0200

    drag and drop

---
+++ tests/DragandDrop.spec.js
@@ -0,0 +1,27 @@
+import {test, expect} from '@playwright/test'
+
+
+test ('Drag And Drop', async ({page})=>{
+
+await page.goto('http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html')
+
+const rome=await page.locator('#box6')
+const italy=await page.locator('#box106')
+
+//Appraoch 1
+// await rome.hover()
+// await page.mouse.down()
+
+// await italy.hover()
+// await page.mouse.up()
+
+//Appraoch 2
+await rome.dragTo(italy)
+
+await page.waitForTimeout(3000)
+
+})
+
+
+
+
