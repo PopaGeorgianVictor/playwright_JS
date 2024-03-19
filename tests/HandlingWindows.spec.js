22d041d3ea195a00b204aaa3ddb71cc87c0cd7b6
Victor Georgian Popa <victor.popa@ad.softeh.ro>
2024-03-19 21:13:14 +0200

    multiple window

---
+++ tests/HandlingWindows.spec.js
@@ -0,0 +1,38 @@
+const {test , expect , chromium} = require('@playwright/test')
+
+test('Handle Pages/Windows' , async () => {
+
+    const browser = await chromium.launch()
+    const context = await browser.newContext()
+
+    const page1 = await context.newPage()
+    const page2 = await context.newPage()
+
+    const allPages = context.pages()
+    console.log('No of pages created:', allPages.length)
+
+    await page1.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await expect(page1).toHaveTitle('OrangeHRM')
+
+    await page2.goto('https://www.orangehrm.com/')
+    await expect(page2).toHaveTitle('Human Resources Management Software | OrangeHRM')
+
+})
+
+test.only('Handle Multiple Pages/Windows' , async () => {
+
+    const browser = await chromium.launch()
+    const context = await browser.newContext()
+
+    const page1 = await context.newPage()
+    await page1.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await expect(page1).toHaveTitle('OrangeHRM')
+
+    const pagePromise = context.waitForEvent('page')
+    await page1.locator('')
+
+
+    
+
+
+})
