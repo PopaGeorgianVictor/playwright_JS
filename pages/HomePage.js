1eca025b15b2c4e4ec4b2e3b53f7955955020e55
Victor Georgian Popa <victor.popa@ad.softeh.ro>
2024-03-19 20:13:39 +0200

    pom

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,5 +1,6 @@
 
 exports.HomePage = class HomePage {
+
 constructor(page) {
     this.page = page
     this.productList ='//*[@id="tbodyid"]/div/div/div/h4/a'
--- tests/Pom.spec.js
+++ tests/Pom.spec.js
@@ -1,5 +1,7 @@
 import {test, expect} from '@playwright/test'
 import { LoginPage } from '../pages/LoginPage'
+import { HomePage } from '../pages/HomePage'
+import { CartPage } from '../pages/CartPage'
 
 test('pom', async ({ page }) => {
 
@@ -7,14 +9,20 @@ test('pom', async ({ page }) => {
 const login = new LoginPage(page)
 await login.gotoLoginPage()
 await login.login('zaheu' , 'test123@')
-
-await page.waitForTimeout(5000)
+await page.waitForTimeout(3000)
 
 
 //Home
-
+const home = new HomePage(page)
+await home.addProductToCart("Nexus 6")
+await page.waitForTimeout(3000)
+await home.gotoCart()
 
 //Cart
+const cart = new CartPage(page)
+await page.waitForTimeout(3000)
+const status = await cart.checkProductInCart('Nexus 6')
+expect(await status).toBe(true)
 
 
 })
