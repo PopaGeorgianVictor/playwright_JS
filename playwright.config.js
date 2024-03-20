96894669f6ba1270d7d69a5b9293d7f7fb17c3af
Victor Georgian Popa <victor.popa@ad.softeh.ro>
2024-03-20 20:41:58 +0200

    config

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,14 @@ module.exports = defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: [['json', {outputFile: 'results.json'}]],
   // reporter: [['junit', {outputFile: 'results.xml'}]],
-     reporter : 'html',
+  // reporter : 'html',
+
+  reporter: [['list'], ['html'],
+            ['junit', {outputFile: 'results.xml' }],
+            ['json', { outputFile: 'results.json' }],
+            ['allure-playwright', {outputFolder: 'my-allure-results'}]
+            ],
+    
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
