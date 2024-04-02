4d2b790fd63613b3ac341ac8a9934925d59afba2
Victor Georgian Popa <victor.popa@ad.softeh.ro>
2024-04-02 20:58:49 +0300

    dom

--- tests/UItest.spec.js
+++ tests/UItest.spec.js
@@ -1,18 +1,33 @@
 const { test, expect } = require('@playwright/test')
 
 
-test('Browser Context Playwright test', async ({browser})=>{
+test.only('Browser Context Playwright test', async ({browser})=>{
+
+    
     const context = await browser.newContext()
     const page = await context.newPage()
+    const userName = page.locator('#username')
+    const signIn = page.locator('#signInBtn')
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
     console.log(await page.title())
 
-//css
-await page.locator('#username').fill('zaheu')  // .type('zaheu')
-await page.locator('#password').fill('zaheu123')
-await page.locator('#signInBtn').click()
+//verify incorect userName
+await userName.fill('wrongUsername')  
+await page.locator('#password').fill('learning')
+await signIn.click()
 console.log(await page.locator("[style*='block']").textContent())
 await expect(await page.locator("[style*='block']")).toContainText('Incorrect')
+
+//corect userName
+await userName.fill('') //empty wrong username
+await userName.fill('rahulshettyacademy')
+await signIn.click()
+
+console.log(await page.locator(".card-body a").first().textContent()) //return first elem on the page / similary: .nth(0)
+
+
+await page.waitForTimeout(3000)
+
 
 
 })
