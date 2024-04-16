491d3abad3bea803de13d0b41efec83536e41030
Victor Georgian Popa <victor.popa@ad.softeh.ro>
2024-04-16 20:14:25 +0300

    waitForLoadState

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -4,9 +4,15 @@ const { test, expect } = require('@playwright/test')
 test.only('Browser Context-Validating Error Login', async ({page})=>{
 
     await page.goto("https://rahulshettyacademy.com/client")
+    await page.locator("#userEmail").fill("anshika@gmail.com")
+    await page.locator("#userPassword").fill("Iamking@000")
+    await page.locator("[value='Login']").click()
+    await page.waitForLoadState('networkidle')
 
+    const titles = await page.locator(".card-body b").allTextContents()
+    console.log(titles)
 
-
+    await page.waitForTimeout(3000)
 
 
 })
