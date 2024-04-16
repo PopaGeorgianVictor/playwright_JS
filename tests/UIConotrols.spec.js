6d744d8f9e1b818df069d063861f953656e85304
Victor Georgian Popa <victor.popa@ad.softeh.ro>
2024-04-16 20:56:26 +0300

    uicontrol

--- tests/UIConotrols.spec.js
+++ tests/UIConotrols.spec.js
@@ -10,18 +10,13 @@ test.only('Browser Context-Validating Error Login', async ({page})=>{
     await dropdown.selectOption("consult")
     await page.locator(".radiotextsty").last().click()
     await page.locator("#okayBtn").click()
+    await expect(page.locator(".radiotextsty").last()).toBeChecked()
+    await page.locator("#terms").click()
+    await expect(page.locator("#terms")).toBeChecked()
+    await page.locator("#terms").uncheck()
+    expect(await page.locator("#terms").isChecked()).toBeFalsy()
     
-
-
-
     //await page.pause() - pause test and open inspector
-
-
-
-
-
-
-
 
 
 })
