779dc3c0bb620c0b1c1ac25ff822f2afb14dacb3
Victor Georgian Popa <popa.georgiani93@gmail.com>
2024-04-18 20:41:14 +0300

    clientapp

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -3,6 +3,7 @@ const { test, expect } = require('@playwright/test')
 
 test('Browser Context-Validating Error Login', async ({page})=>{
 
+    const products = page.locator(".card-body")
     await page.goto("https://rahulshettyacademy.com/client")
     await page.locator("#userEmail").fill("anshika@gmail.com")
     await page.locator("#userPassword").fill("Iamking@000")
@@ -11,6 +12,11 @@ test('Browser Context-Validating Error Login', async ({page})=>{
 
     const titles = await page.locator(".card-body b").allTextContents()
     console.log(titles)
+    const count = await products.count()
+    for(let i=0; i < count ; ++i)
+    {
+        products.nth(i)
+    }
 
     await page.waitForTimeout(3000)
 
