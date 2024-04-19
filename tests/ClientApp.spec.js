e3b366c45c643efda88d1d04207dab24175bc358
Victor Georgian Popa <popa.georgiani93@gmail.com>
2024-04-19 14:39:06 +0300

    clientapp

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -3,10 +3,11 @@ const { test, expect } = require('@playwright/test')
 
 test.only('Browser Context-Validating Error Login', async ({page})=>{
 
-    const productName = 'Zara Coat 3'
+    const email = "anshika@gmail.com"
+    const productName = 'ADIDAS ORIGINAL'
     const products = page.locator(".card-body")
-    await page.goto("https://rahulshettyacademy.com/client")
-    await page.locator("#userEmail").fill("anshika@gmail.com")
+    await page.goto("https://rahulshettyacademy.com/client/")
+    await page.locator("#userEmail").fill(email)
     await page.locator("#userPassword").fill("Iamking@000")
     await page.locator("[value='Login']").click()
     await page.waitForLoadState('networkidle')
@@ -23,10 +24,11 @@ test.only('Browser Context-Validating Error Login', async ({page})=>{
           break
         }
     }
+    await page.pause()
 
     await page.locator("[routerlink*='cart']").click()
     await page.locator("div li").first().waitFor()
-    const bool = await page.locator("h3:has-text('Zara Coat 3')").isVisible()
+    const bool = await page.locator("h3:has-text('ADIDAS ORIGINAL')").isVisible()
     expect(bool).toBeTruthy()
     await page.locator("text=Checkout").click()
     await page.locator("[placeholder*='Country']").fill("ind", {delay: 100}) // delay typing to show up suggestions (slow down typing)
@@ -36,12 +38,19 @@ test.only('Browser Context-Validating Error Login', async ({page})=>{
     for (let i = 0; i< optionsCount; ++i)
     {
       text = await dropdown.locator("button").nth(i).textContent()
-      if(text === "India")
+      if(text.trim() === "India") // trim to eliminate space or write ' India'  with space like in the js or use java methods text.includes("India")
       {
         await dropdown.locator("button").nth(i).click()
         break
       }
     }
+
+    await expect(page.locator(".user__name [type='text']")).toHaveText(email)
+    await page.locator(".action__submit").click()
+    await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ")
+    const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent()
+    console.log(orderId)
+
   
    // await page.pause()
 
