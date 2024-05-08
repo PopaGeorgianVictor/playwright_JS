61a549ba6f502e1e623371ff749b88980aabaaef
Victor Georgian Popa <popa.georgiani93@gmail.com>
2024-05-08 17:03:23 +0300

    frame

--- tests/Validation.spec.js
+++ tests/Validation.spec.js
@@ -13,8 +13,17 @@ await expect(page.locator("#displayed-text")).toBeVisible()
 await page.locator("#hide-textbox").click()
 await expect(page.locator("#displayed-text")).toBeHidden()
 
+// Alert
 page.on('dialog' , dialog => dialog.accept())
 await page.locator("#confirmbtn").click()
+
+// Mouse hover
 await page.locator("#mousehover").hover()
+
+// Frame
+const framesPage = page.frameLocator("#courses-iframe")
+await framesPage.locator("li a[href*='lifetime-access']:visible").click() // focus only on the visible locator
+const textCheck = await framesPage.locator(".text h2").textContent()
+console.log(textCheck.split(" ")[1]) //extract only numbers in the string
 
 })
