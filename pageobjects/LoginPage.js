71ac91e1066c84f8faf4fe38cf0a1d3f37c2647d
Victor Georgian Popa <popa.georgiani93@gmail.com>
2024-05-27 17:10:00 +0300

    pomanager

--- pageobjects/LoginPage.js
+++ pageobjects/LoginPage.js
@@ -1,29 +1,27 @@
-class LoginPage{
+class LoginPage {
 
 constructor(page)
 {
-    this.page = page
-    this.signInbutton = page.locator("[value='Login']")
-    this.userName = page.locator("#userEmail")
-    this.password = page.locator("#userPassword")
+    this.page = page;
+    this.signInbutton= page.locator("[value='Login']");
+    this.userName = page.locator("#userEmail");
+    this.password = page.locator("#userPassword");
 
 }
 
 async goTo()
-
 {
-    await this.page.goto("https://rahulshettyacademy.com/client/")
+    await this.page.goto("https://rahulshettyacademy.com/client");
 }
 
-async validLogin(userName,password)
-    {
-        await this.userName.fill(userName)
-        await this.password.fill(password)
-        await this.signInbutton.click()
-        await this.page.waitForLoadState('networkidle')
-    }
-
-
+async validLogin(username,password)
+{
+    await  this.userName.type(username);
+     await this.password.type(password);
+     await this.signInbutton.click();
+     await this.page.waitForLoadState('networkidle');
 
 }
-module.exports = {LoginPage}
+
+}
+module.exports = {LoginPage};
