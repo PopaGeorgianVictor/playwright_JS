106c99b6a6798c8fee447defb404002b025ecf43
Victor Georgian Popa <popa.georgiani93@gmail.com>
2024-05-28 16:44:38 +0300

    pom

--- tests/ClientAppPOM.spec.js
+++ tests/ClientAppPOM.spec.js
@@ -1,24 +1,23 @@
 const { test, expect } = require('@playwright/test')
 const {POManager} =  require('../pageobjects/POManager')
+const dataset = JSON.parse(JSON.stringify(require("../utils/placeorderTestData.json"))) //json ->convert to string ->convert to js object
 
 
 test('ClientApp', async ({page})=>{
 
   const poManager = new POManager(page)
   //js file- Login js, DashboardPage
-   const username = "anshika@gmail.com"
-   const password = "Iamking@000"
-   const productName = 'Zara Coat 4'
+
    const products = page.locator(".card-body")
    const loginPage = poManager.getLoginPage()
    await loginPage.goTo()
-   await loginPage.validLogin(username,password)
+   await loginPage.validLogin(dataset.username,dataset.password)
    const dashboardPage = poManager.getDashboardPage()
-   await dashboardPage.searchProductAddCart(productName)
+   await dashboardPage.searchProductAddCart(dataset.productName)
    await dashboardPage.navigateToCart()
 
   const cartPage = poManager.getCartPage()
-  await cartPage.VerifyProductIsDisplayed(productName)
+  await cartPage.VerifyProductIsDisplayed(dataset.productName)
   await cartPage.Checkout()
 
   const ordersReviewPage = poManager.getOrdersReviewPage()
